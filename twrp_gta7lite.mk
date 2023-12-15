db9eccb5c01894aa1a645049b0134ad141863a6f
DY3413 <milosix3413@gmail.com>
2023-12-15 14:58:13 +0900

    Inherit device configuration
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- twrp_gta7lite.mk
+++ twrp_gta7lite.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# Inherit device configuration
+$(call inherit-product, device/samsung/gta7lite/device.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gta7lite
 PRODUCT_NAME := twrp_gta7lite
