c6ffc2888c4d01e40f44487198cb7832ad20de21
DY3413 <milosix3413@gmail.com>
2023-12-15 14:56:29 +0900

    Inherit common configurations
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- twrp_gta7lite.mk
+++ twrp_gta7lite.mk
@@ -5,6 +5,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gta7lite
 PRODUCT_NAME := twrp_gta7lite
