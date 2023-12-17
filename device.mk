e765f947c2fe9ed41987947e40af8c298df783da
DY3413 <milosix3413@gmail.com>
2023-12-17 21:53:39 +0900

    Set PRODUCT_USE_DYNAMIC_PARTITIONS to true
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- device.mk
+++ device.mk
@@ -5,6 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Dynamic partition
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
